fb7b3f208467724cbeef054d1a76473ebabc88cb
tanuj <tanuj.com>
2021-05-21 12:18:03 +0530

    importing required libraries

---
+++ 3_Implementation/src/main.py
@@ -0,0 +1,14 @@
+#-------importing required libraries---------
+
+import tkinter
+from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk
+import datetime as dt
+import time
+from time import strftime
+import sys
+import math
+import random
+from tkinter import messagebox
+import os
